b374e6e13968ade593c8870c3287e04290dec8e5
1333123as <125147417+1333123as@users.noreply.github.com>
2023-05-07 06:29:13 -0600

    Add files via upload

---
+++ 1333123_Bisiesto/Program.cs
@@ -0,0 +1,16 @@
+﻿int año = -1;
+
+if (año > 0 && año %4 == 0)
+{
+    Console.WriteLine("El año es bisiesto");
+}
+if (año < 0)
+{
+    Console.WriteLine("El año no puede ser menor a 0");
+}
+else 
+{
+    Console.WriteLine("El año no es bisiesto");
+}
+
+
